be3d670aaecd5ea7b60dc969c8648f4f3c3bb893
dongzoolee <nant0313@gmail.com>
2021-07-18 02:31:31 +0900

    Format code

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
 const express = require('express');
 const app = express();
-const getData = require('./utils/getData');
+const cofo = require('./utils/cofo');
 const sql = require('./utils/sql');
-const fs = require('fs')
-const { RANK_EXP, TIER_COLOR } = require('./utils/cofoRatingInfo')
-const { svgDataForLGM, svgDataForGeneralRating } = require('./utils/SVGs')
+const { svgDataForLGM, svgDataForGeneralRating } = require('./utils/SVGs');
 
-app.use('/', (req, res) => {
+app.use('/', async (req, res) => {
   res.setHeader('Content-Type', 'image/svg+xml');
   res.setHeader('Cache-Control', 'public, max-age=3600');
 
+  // handle errors
   const id = req.query.id;
   if (!id) return res.send('<svg>handle cannot be empty</svg>');
-  const handleFormat = id.replace(/[a-zA-Z0-9-_]/g,'')
-  if(handleFormat) return res.send('<svg>handle should only contain Latin letters, digits, underscore or dash characters</svg>');
-  
+  const handleFormat = id.replace(/[a-zA-Z0-9-_]/g, '')
+  if (handleFormat) return res.send('<svg>handle should only contain Latin letters, digits, underscore or dash characters</svg>');
+
+  // get user rank, rating
+  const { status, userInfo } = await getUserInfo(id)
+  if (status === "FAILED") return res.send('<svg>handle not available</svg>')
+
+  // handle errors
+  if (!userInfo.rank) {
+    return res.send('<svg>rating not available</svg>');
+  }
+
+  // render svg
+  if (userInfo.rank === "legendary grandmaster") {
+    res.send(svgDataForLGM(userInfo))
+  } else
+    res.send(svgDataForGeneralRating(userInfo))
+});
+
+const getUserInfo = async (id) => {
   const userInfo = {
     id: "",
     rank: "",
@@ -22,11 +38,14 @@ app.use('/', (req, res) => {
     maxRank: "",
     maxRating: ""
   };
-  
+
   const { exists, rows } = await sql.getClient(id)
   if (!exists) {
     const { status, result } = await cofo.getTier(id)
+    // not available handle in codeforces
     if (status === "FAILED") return { status, userInfo }
+
+    // insert newly added handle data
     await sql.insertData(result[0])
     userInfo.id = id;
     userInfo.rank = result[0].rank;
@@ -34,32 +53,16 @@ app.use('/', (req, res) => {
     userInfo.maxRank = result[0].maxRank;
     userInfo.maxRating = result[0].maxRating;
   } else {
+    // showing existing data
     userInfo.id = id;
     userInfo.rank = rows[0].rank;
     userInfo.rating = rows[0].rating;
     userInfo.maxRank = rows[0].maxRank;
     userInfo.maxRating = rows[0].topRating;
   }
+  return { status: "SUCCESS", userInfo }
+}
 
-        userInfo.id = id;
-        userInfo.rank = pInfo[0].rank;
-        userInfo.rating = pInfo[0].rating;
-        userInfo.maxRank = pInfo[0].maxRank;
-        userInfo.maxRating = pInfo[0].maxRating;
-        // 중복호출시 userInfo.rank가 null인 상태로 호출된다
-        // 왜 중복호출될까..
-        if (!RANK_EXP[userInfo.rank]) {
-          return res.send('<svg>rating not available</svg>');
-        }
-
-        if (userInfo.rank === "Legendary Grandmaster") {
-          res.send(svgDataForLGM(userInfo))
-        } else
-          res.send(svgDataForGeneralRating(userInfo))
-      });
-    })
-  })
-});
 const server = app.listen(2021, () => {
   console.log('server has started on 2021');
 })
