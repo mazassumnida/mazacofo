9743b1047b0d0052eeda99c48877730f9e042f4b
dongzoolee <nant0313@gmail.com>
2021-07-18 01:19:38 +0900

    Add func && reformat code

--- utils/sql.js
+++ utils/sql.js
@@ -1,5 +1,5 @@
 const sql = module.exports = {};
-const mysql2 = require('mysql2');
+const mysql2 = require('mysql2/promise');
 const dotenv = require('dotenv');
 dotenv.config()
 const connection = mysql2.createPool({
@@ -8,10 +8,36 @@ const connection = mysql2.createPool({
     password: process.env.MYSQL_PW,
     database: process.env.MYSQL_DB
 })
-sql.insertId = (id) => {
-    if (!id) return;
-    connection.query('INSERT INTO clients(handle) VALUES(?)', [id], (err, res, fields) => {
-        if (err) console.log(err);
-        else console.log(res)
-    })
+sql.insertData = async ({ handle, rank, rating, maxRank, maxRating }) => {
+    if (!handle) return;
+    try {
+        const [rows] = await connection.query('INSERT INTO clients(handle, rank, rating, maxRank, topRating) VALUES(?)', [handle, rank, rating, maxRank, maxRating])
+    } catch (error) {
+        console.error(error)
+    }
+}
+sql.updateRank = async ({ handle, rank, rating, maxRank, maxRating }) => {
+    if (!handle) return;
+    try {
+        const [rows] = await connection.query('UPDATE clients SET rank=?, rating=?, maxRank=?, topRating=? WHERE handle=?', [rank, rating, maxRank, maxRating, handle])
+    } catch (error) {
+        console.error(error)
+    }
+}
+sql.checkIfExists = async (id) => {
+    try {
+        const [rows] = await connection.query(`SELECT * FROM clients WHERE handle=?`, [id])
+        const ret = rows?.length ? true : false
+        return ret
+    } catch (error) {
+        console.error(error)
+    }
+}
+sql.getClients = async () => {
+    try {
+        const [rows] = await connection.query(`SELECT * FROM clients`)
+        return rows
+    } catch (error) {
+        console.error(error)
+    }
 }
