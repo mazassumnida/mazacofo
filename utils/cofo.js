a8a96d69db02394f9c8857071f45d95a7c2cee87
dongzoolee <nant0313@gmail.com>
2021-07-18 01:38:29 +0900

    Fix error

--- utils/cofo.js
+++ utils/cofo.js
@@ -7,7 +7,7 @@ cofo.getTier = async (id) => {
     try {
         const url = "https://codeforces.com/api/user.info?handles=" + id;
         const { data } = await axios.get(url)
-        return JSON.parse(data)
+        return data
     } catch (error) {
         console.error(error)
         return { status: "FAILED", result: [] }
