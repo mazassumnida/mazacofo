8004d969f05d246a9ca3027a494ddc6c9f97dc98
dongzoolee <nant0313@gmail.com>
2021-07-18 01:19:27 +0900

    Create file with new logic

---
+++ utils/cofo.js
@@ -0,0 +1,15 @@
+const cofo = module.exports = {};
+const fs = require('fs');
+const axios = require('axios');
+const path = require('path');
+
+cofo.getTier = async (id) => {
+    try {
+        const url = "https://codeforces.com/api/user.info?handles=" + id;
+        const { data } = await axios.get(url)
+        return JSON.parse(data)
+    } catch (error) {
+        console.error(error)
+        return { status: "FAILED", result: [] }
+    }
+}
