a84650945cba5fa7d5ea91622e0ab8d9595f9ddb
DongZoo Lee <dongzoo@kodebox.io>
2022-01-30 22:50:14 +0900

    Delete unused imports

--- utils/cofo.js
+++ utils/cofo.js
@@ -1,15 +1,13 @@
 const cofo = module.exports = {};
-const fs = require('fs');
 const axios = require('axios');
-const path = require('path');
 
 cofo.getTier = async (id) => {
-    try {
-        const url = "https://codeforces.com/api/user.info?handles=" + id;
-        const { data } = await axios.get(url)
-        return data
-    } catch (error) {
-        console.error(error)
-        return { status: "FAILED", result: [] }
-    }
+  try {
+    const url = "https://codeforces.com/api/user.info?handles=" + id;
+    const { data } = await axios.get(url)
+    return data
+  } catch (error) {
+    console.error(error)
+    return { status: "FAILED", result: [] }
+  }
 }
