4e4050c352b59569c2520782d80a181c591db635
DongZoo Lee <dongzoo@kodebox.io>
2022-07-05 02:19:24 +0900

    Add logging for codeforces API exceptions

--- utils/cofo.js
+++ utils/cofo.js
@@ -1,6 +1,8 @@
 const cofo = (module.exports = {});
 const axios = require("axios");
 
+const logger = require("./logger");
+
 cofo.getTier = async (id) => {
   try {
     const url = "https://codeforces.com/api/user.info?handles=" + id;
@@ -8,6 +10,7 @@ cofo.getTier = async (id) => {
     return data;
   } catch (error) {
     console.error(error);
+    logger.exception(error.response.data.comment);
     return { status: "FAILED", result: [] };
   }
 };
