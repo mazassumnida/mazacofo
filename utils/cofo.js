86ec8760ac96538c47cb4217918adc08b9eefd25
DongZoo Lee <dongzoo@kodebox.io>
2022-12-31 12:53:19 +0900

    Add a feature checking if there's any changed handle

--- utils/cofo.js
+++ utils/cofo.js
@@ -4,13 +4,44 @@ const axios = require("axios");
 const logger = require("./logger");
 
 cofo.getTier = async (id) => {
+  const url = "https://codeforces.com/api/user.info?handles=" + id;
+
   try {
-    const url = "https://codeforces.com/api/user.info?handles=" + id;
     const { data } = await axios.get(url);
     return data;
   } catch (error) {
     console.error(error);
+
+    const { ok, changedHandle } = await checkForChangedHandle(id);
+    if (ok) {
+      return await cofo.getTier(changedHandle);
+    }
+
     logger.exception(error.response.data.comment);
+
     return { status: "FAILED", result: [] };
   }
+};
+
+const checkForChangedHandle = async (handle) => {
+  try {
+    await axios.head(`https://codeforces.com/profile/${handle}`, {
+      maxRedirects: 0,
+    });
+  } catch (e) {
+    const response = e.response;
+
+    if (response.status === 302) {
+      const movedURL = response.headers.location;
+      return {
+        ok: true,
+        changedHandle: movedURL.replace("https://codeforces.com/profile/", ""),
+      };
+    }
+  }
+
+  return {
+    ok: false,
+    changedHandle: null,
+  };
 };
