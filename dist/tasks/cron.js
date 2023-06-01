399bf05333249793057f84fac3881ce3b1ffffb2
DongZoo Lee <dongzoo@kodebox.io>
2023-06-02 00:42:51 +0900

    Move service logics into a src folder

---
+++ dist/tasks/cron.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const CronJob = require("cron").CronJob;
+
+// Update Codeforces data every hour
+const cofoTask = require("./cofoTask");
+const job = new CronJob("0 * */1 * * *", cofoTask.updateCofo);
+job.start();
