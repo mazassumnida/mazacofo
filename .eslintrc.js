5dad8515576843e917bf401f74c02395d4d80388
DongZoo Lee <dongzoo@kodebox.io>
2023-08-15 03:23:18 +0900

    Initialize eslint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:import/recommended",
+    "airbnb-base",
+    "airbnb-typescript/base",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.js"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+  },
+  ignorePatterns: ["node_modules/", "dist/"],
+  rules: {
+    "@typescript-eslint/quotes": "off",
+    "operator-linebreak": "off",
+  },
+};
